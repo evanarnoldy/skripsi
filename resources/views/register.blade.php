d54af2b368fabcb82614c5ff4abc0b08eb61af57
evanarnoldy <septariantoevan@gmail.com>
2020-02-27 21:19:37 +0700

    menambahkan file ke repo

---
+++ resources/views/register.blade.php
@@ -0,0 +1,12 @@
+@extends('layout/login')
+
+@section('form')
+    <h3> Daftar Sebagai :</h3>
+    <form>
+        <div class="tombol">
+            <button type="button" class="btn btn-primary btn-lg">Siswa</button>
+            <button type="button" class="btn btn-primary btn-lg">Guru</button>
+        </div>
+    </form>
+
+@endsection
