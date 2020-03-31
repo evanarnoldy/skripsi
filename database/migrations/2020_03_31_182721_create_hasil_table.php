f5a0affcac46eb142f8ad223f1771d747fa36272
evanarnoldy <septariantoevan@gmail.com>
2020-04-01 01:52:03 +0700

    perbaikan tabel

---
+++ database/migrations/2020_03_31_182721_create_hasil_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHasilTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hasil', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('student_id');
+            $table->string('skor');
+            $table->string('kesimpulan');
+            $table->string('bulan');
+            $table->string('tahun');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hasil');
+    }
+}
