e62a3e2d5678db3c5b9ca5c08212fb6c8e4dddc6
evanarnoldy <septariantoevan@gmail.com>
2020-07-24 16:54:28 +0700

    menambahkan grafik khusus kesehatan mental

--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -7,6 +7,12 @@
         </div>
 
         <div class="container">
+            <div id="chartksh">
+
+            </div>
+        </div>
+
+        <div class="container">
             <div id="chart">
 
             </div>
@@ -17,10 +23,49 @@
 @section('footer')
     <script src="https://code.highcharts.com/highcharts.js"></script>
     <script>
+        $('#chartksh').highcharts({
+            chart: {
+                type: 'column'
+            },
+            title: {
+                text: 'Grafik Kesehatan Mental {{Auth::user()->nama}} Tahun {{date('Y')}}'
+            },
+            xAxis: {
+                categories: {!! json_encode($bulanksh) !!},
+                crosshair: true
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: 'Nilai'
+                }
+            },
+            tooltip: {
+                headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                    '<td style="padding:0"><b>{point.y}</b></td></tr>',
+                footerFormat: '</table>',
+                shared: true,
+                useHTML: true
+            },
+            plotOptions: {
+                column: {
+                    pointPadding: 0.2,
+                    borderWidth: 0
+                }
+            },
+            series: [{
+                name: 'Kesehatan Mental',
+                data: [{{implode(',',$nilai)}}]
+
+            }]
+        });
+
+        //chart prestasi
         Highcharts.chart('chart', {
 
             title: {
-                text: 'Grafik Kesehatan Mental {{Auth::user()->nama}} Tahun {{date('Y')}}'
+                text: 'Grafik Prestasi Belajar {{Auth::user()->nama}} Tahun {{date('Y')}}'
             },
 
             yAxis: {
@@ -50,9 +95,6 @@
             },
 
             series: [{
-                name: 'Kesehatan Mental',
-                data: [{{implode(',',$nilai)}}],
-            },{
                 name: 'Prestasi Belajar',
                 data: [{{implode(',',$prestasi)}}],
             }],
